efa564f4d1b8337ffdc38dcd31c429997ed79263
SurabhiK13 <surabhik132002@outlook.com>
2024-10-05 12:39:05 +0530

    Add project files.

---
+++ FishUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaticPetShop
+{
+    public class FishUtility : Fish
+    {
+        public void AddFish(string species, double pricePerFish)
+        {
+            Species = species;
+            PricePerFish = pricePerFish;
+
+        }
+        public bool BuyFish()
+        {
+            if (Species == "Clownfish" || Species == "Goldfish")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double CalculatePrice(int numberOfFishes)
+        {
+            double Additionalcharges = 0;
+            if (Species == "Clownfish")
+            {
+                Additionalcharges = 100;
+            }
+            else if (Species == "Goldfish")
+            {
+                Additionalcharges = 150;
+            }
+            double totalPrice = (PricePerFish * numberOfFishes) + Additionalcharges;
+            return totalPrice;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AquaticPetShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fish fish = new Fish();
+            FishUtility fishes = new FishUtility();
+            Console.WriteLine("Enter The Species To Buy");
+            fishes.Species = Console.ReadLine();
+            Console.WriteLine("Enter The Price Per Fish");
+            fishes.PricePerFish = Convert.ToDouble(Console.ReadLine());
+            fishes.AddFish(fishes.Species, fishes.PricePerFish);
+            if (fishes.BuyFish())
+            {
+                Console.WriteLine("Enter The Number Of You Fishes To Buy");
+                int numberOfFishes = Convert.ToInt32(Console.ReadLine());
+                double totalCost = fishes.CalculatePrice(numberOfFishes);
+                Console.WriteLine($"Total COst is {totalCost}");
+            }
+            else
+            {
+                Console.WriteLine($"{fishes.Species} species Not Found");
+            }
+        }
+
+    }
+}
